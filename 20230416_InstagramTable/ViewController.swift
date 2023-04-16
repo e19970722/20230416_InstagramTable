f63946cf70e4e248d96ef6b653a8a623cc08ada5
Yen Lin <e19970722@gmail.com>
2023-04-16 23:35:32 +0800

    Initial Commit

---
+++ 20230416_InstagramTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20230416_InstagramTable
+//
+//  Created by Yen Lin on 2023/4/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
